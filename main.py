aabf942a956cd707116f770b60b5fd716a17db62
Yegor-Vasilevskii <56190514+Yegor-Vasilevskii@users.noreply.github.com>
2019-10-28 23:00:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-#Go_ahead
+#Developers:
+#Mahanov S (%), Pestretsova M (%), Pokareva C(%), Vasilevskiy Y (%)
+#Let's say hello to our guests!
+print('Здравствуйте! Добро пожаловать в нашу добрую и славную налоговую инспекцию')
+print('Давайте вместе расситаем, сколько вы нам должны!')
+print('Введте, пожалуйста, Ваш доход (начните с годового):')
+income = float(input(''))
