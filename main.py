045e3d93f1719dff982300c95b421bd5c3194ce1
ChristinaPokareva <56190489+ChristinaPokareva@users.noreply.github.com>
2019-10-31 23:38:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Developers:
-#Mahanov S (%), Pestretsova M (%), Pokareva C(%), Vasilevskiy Y (%)
+#Mahanov S (%), Pestretsova M (%), Pokareva C(20%), Vasilevskiy Y (%)
 #Let's say hello to our guests!
 print('Здравствуйте! Добро пожаловать в нашу добрую и славную налоговую инспекцию')
 print('Давайте вместе расситаем, сколько Вы нам должны!')
@@ -27,5 +27,27 @@ if type_number == 1:
             return 0.1 * 9075 + (36900 - 9075) * 0.15 + (89350 - 36900) * 0.25 + (186350 - 89350) * 0.28 + (405100 - 186350) * 0.33 + (406750 - 405100) * 0.35 + (b - 406750) * 0.396
     tax_for_lonely = tax_income(income)
     print(tax_for_lonely)
-else:
-    pass #Напишите продолжение позже
+
+#Pestretsova:    
+    
+#Pokareva: I'll write about lonely parent, if type_number == 3
+    else:
+    def tax_income(b):
+        if b > 0 and b <= 12950:
+            return 0.1 * b
+        elif b > 12950 and b <= 49400:
+            return 0.1 * 12950 + (b - 12951) * 0.15
+        elif b > 49400 and b <= 127550:
+            return 0.1 * 12950 + (49400 - 12951) * 0.15 + (b - 49401) * 0.25
+        elif b > 127550 and b <= 206600:
+            return 0.1 * 12950 + (49400 - 12951) * 0.15 + (127550 - 49401) * 0.25 + (b - 127551) * 0.28
+        elif b > 206600 and b <= 405100:
+            return 0.1 * 12950 + (49400 - 12951) * 0.15 + (127550 - 49401) * 0.25 + (186350 - 127551) * 0.28 + (b - 206601) * 0.33
+        elif b > 405100 and b <= 432200:
+            return 0.1 * 12950 + (49400 - 12951) * 0.15 + (127550 - 49401) * 0.25 + (186350 - 127551) * 0.28 + (405100 - 206601) * 0.33 + (b - 405101) * 0.35
+        else:
+            return 0.1 * 12950 + (49400 - 12951) * 0.15 + (127550 - 49401) * 0.25 + (186350 - 127551) * 0.28 + (405100 - 206601) * 0.33 + (432200 - 405101) * 0.35 + (b - 432201) * 0.396
+    tax_for_lonely_parents = tax_income(income)
+    print(tax_for_lonely_parents)
+       
+        pass #Напишите продолжение позже
