10c658d18c8f7f00334e13d4f9af77516ee51808
Stepan <56470412+StepanDt@users.noreply.github.com>
2019-11-01 22:59:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ income = 0
 for element in month:
     income += element
 print('Введите ваше семейное положение: 1 - Холост(а); 2 - Женат(а); 3 - отец/мать одиночка')
-number_type = input()
+number_type = int(input())
 def tax_for_lonely(b):
     if b > 0 and b <= 9075:
         return 0.1 * b
@@ -36,9 +36,6 @@ def tax_for_lonely(b):
         return 0.1 * 9075 + (36900 - 9075) * 0.15 + (89350 - 36900) * 0.25 + (186350 - 89350) * 0.28 + (405100 - 186350) * 0.33 + (b - 405100) * 0.35
     else:
         return 0.1 * 9075 + (36900 - 9075) * 0.15 + (89350 - 36900) * 0.25 + (186350 - 89350) * 0.28 + (405100 - 186350) * 0.33 + (406750 - 405100) * 0.35 + (b - 406750) * 0.396
-   
-    
-#Pestretsova:   I'll write about  married couples, if type_number == 2
 def tax_for_married_couples(b):
     if b > 0 and b <= 18150:
         return 0.1 * b
@@ -57,11 +54,6 @@ def tax_for_married_couples(b):
     else:
         return 0.1 * 18150 + (73800 - 18150) * 0.15 + (148850 - 73800) * 0.25 + (226850 - 148850) * 0.28 + (
                             405100 - 226850) * 0.33 + (457600 - 405100) * 0.35 + (b - 457600) * 0.396
-
-
-          
-#Pokareva: I'll write about lonely parent, if type_number == 3
-    
 def  tax_for_lonely_parents(b):
     if b > 0 and b <= 12950:
         return 0.1 * b
@@ -77,4 +69,8 @@ def  tax_for_lonely_parents(b):
         return 0.1 * 12950 + (49400 - 12951) * 0.15 + (127550 - 49401) * 0.25 + (186350 - 127551) * 0.28 + (405100 - 206601) * 0.33 + (b - 405101) * 0.35
     else:
         return 0.1 * 12950 + (49400 - 12951) * 0.15 + (127550 - 49401) * 0.25 + (186350 - 127551) * 0.28 + (405100 - 206601) * 0.33 + (432200 - 405101) * 0.35 + (b - 432201) * 0.396
+tax_type_name = [tax_for_lonely(income),tax_for_married_couples(income),tax_for_lonely_parents(income)]
+tax_income = tax_type_name[number_type - 1]
+print('Поздравляем!!!')
+print('Ваш налог составил',tax_income,'рублей')
     
